6e753a72462b2a4e12f097a343e333e1a910d1ba
Olavo Nascimento <olavo.nascimento4@gmail.com>
2022-02-11 17:41:13 -0300

    Adicionar tabela scripts.
    Closes #9.

--- banco_de_dados.sql
+++ banco_de_dados.sql
@@ -1,16 +1,16 @@
-CREATE SCHEMA IF NOT EXISTS t2;
+CREATE SCHEMA IF NOT EXISTS projetobd;
 
-CREATE TABLE IF NOT EXISTS t2.destilaria
+CREATE TABLE IF NOT EXISTS projetobd.destilaria
 (
     nome VARCHAR(50),
     fundacao DATE NOT NULL,
     pais_origem_nome VARCHAR(50) NOT NULL,
 	CONSTRAINT pk_destilaria PRIMARY KEY (nome),
     CONSTRAINT fk_destilaria_pais_origem FOREIGN KEY (pais_origem_nome)
-        REFERENCES t2.pais_origem (nome)
+        REFERENCES projetobd.pais_origem (nome)
 );
 
-CREATE TABLE IF NOT EXISTS t2.loja
+CREATE TABLE IF NOT EXISTS projetobd.loja
 (
     nome VARCHAR(50),
     url VARCHAR(50) NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS t2.loja
 	CONSTRAINT pk_loja PRIMARY KEY (nome)
 );
 
-CREATE TABLE IF NOT EXISTS t2.whisky
+CREATE TABLE IF NOT EXISTS projetobd.whisky
 (
     id SERIAL,
     idade INT NOT NULL,
@@ -29,13 +29,26 @@ CREATE TABLE IF NOT EXISTS t2.whisky
     pais_origem_nome VARCHAR(50) NOT NULL,
 	CONSTRAINT pk_whisky PRIMARY KEY (id),
     CONSTRAINT fk_whisky_destilaria FOREIGN KEY (destilaria_nome)
-        REFERENCES t2.destilaria (nome),
+        REFERENCES projetobd.destilaria (nome),
     CONSTRAINT fk_whisky_pais_origem FOREIGN KEY (pais_origem_nome)
-        REFERENCES t2.pais_origem (nome),
+        REFERENCES projetobd.pais_origem (nome),
    CHECK (preco >= 0 AND preco_desconto >= 0 AND teor_alcolico >= 0)
 );
 
-CREATE TABLE IF NOT EXISTS t2.historico
+CREATE TABLE IF NOT EXISTS projetobd.loja_vende_whisky
+(
+    whisky_id INT,
+    loja_nome VARCHAR(50),
+    preco DECIMAL(2) NOT NULL,
+    preco_desconto DECIMAL(2),
+	CONSTRAINT pk_loja_vende_whisky PRIMARY KEY (whisky_id, loja_nome),
+    CONSTRAINT fk_loja_vende_whisky_whisky FOREIGN KEY (whisky_id)
+        REFERENCES projetobd.whisky (id),
+    CONSTRAINT fk_loja_vende_whisky_loja FOREIGN KEY (loja_nome)
+        REFERENCES projetobd.loja (nome)
+);
+
+CREATE TABLE IF NOT EXISTS projetobd.historico
 (
     whisky_id INT,
     loja_nome VARCHAR(50),
@@ -44,32 +57,41 @@ CREATE TABLE IF NOT EXISTS t2.historico
     acessado_em DATE NOT NULL,
 	CONSTRAINT pk_historico PRIMARY KEY (whisky_id, loja_nome, acessado_em),
     CONSTRAINT fk_historico_whisky FOREIGN KEY (whisky_id)
-        REFERENCES t2.whisky (id),
+        REFERENCES projetobd.whisky (id),
     CONSTRAINT fk_historico_loja FOREIGN KEY (loja_nome)
-        REFERENCES t2.loja (nome)
+        REFERENCES projetobd.loja (nome)
 );
 
-
-CREATE TABLE IF NOT EXISTS t2.pais_origem
+CREATE TABLE IF NOT EXISTS projetobd.pais_origem
 (
     nome VARCHAR(50),
     continente VARCHAR(30),
     CONSTRAINT fk_pais_origem PRIMARY KEY(nome)
 );
 
-CREATE TABLE IF NOT EXISTS t2.ingrediente
+CREATE TABLE IF NOT EXISTS projetobd.ingrediente
 (
     nome VARCHAR(50),
     CONSTRAINT pk_ingrediente PRIMARY KEY (nome)
 );
 
-CREATE TABLE IF NOT EXISTS t2.destilaria_utiliza_ingrediente
+CREATE TABLE IF NOT EXISTS projetobd.destilaria_utiliza_ingrediente
 (
     destilaria_nome VARCHAR(50),
     ingrediente_nome VARCHAR(50),
     CONSTRAINT pk_destilaria_utiliza_ingrediente PRIMARY KEY (destilaria_nome, ingrediente_nome),
     CONSTRAINT fk_destilaria_utiliza_ingrediente_destilaria FOREIGN KEY (destilaria_nome)
-        REFERENCES t2.destilaria (nome),
+        REFERENCES projetobd.destilaria (nome),
     CONSTRAINT destilaria_utiliza_ingrediente_ingrediente FOREIGN KEY (ingrediente_nome)
-        REFERENCES t2.ingrediente (nome)
+        REFERENCES projetobd.ingrediente (nome)
+);
+
+CREATE TABLE IF NOT EXISTS projetobd.script
+(
+    loja_nome VARCHAR(50),
+    data_insercao DATE,
+    codigo TEXT,
+	CONSTRAINT pk_loja_vende_whisky PRIMARY KEY (loja_nome, data_insercao),
+    CONSTRAINT fk_loja_vende_whisky_loja FOREIGN KEY (loja_nome)
+        REFERENCES projetobd.loja (nome)
 );
