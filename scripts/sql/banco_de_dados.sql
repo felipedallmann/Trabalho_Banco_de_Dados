00ee5d581fb2587ba4610721dce1522641249418
Olavo Nascimento <olavo.nascimento4@gmail.com>
2022-02-20 16:18:07 -0300

    Substituir DATE por TIMESTAMP.

--- scripts/sql/banco_de_dados.sql
+++ scripts/sql/banco_de_dados.sql
@@ -2,14 +2,14 @@ CREATE SCHEMA IF NOT EXISTS projetobd;
 
 CREATE TABLE IF NOT EXISTS projetobd.pais_origem
 (
-    nome VARCHAR(50),
+    nome TEXT,
     CONSTRAINT pk_pais_origem PRIMARY KEY(nome)
 );
 
 CREATE TABLE IF NOT EXISTS projetobd.destilaria
 (
-    nome VARCHAR(50),
-    pais_origem_nome VARCHAR(50) NOT NULL,
+    nome TEXT,
+    pais_origem_nome TEXT,
 	CONSTRAINT pk_destilaria PRIMARY KEY (nome),
     CONSTRAINT fk_destilaria_pais_origem FOREIGN KEY (pais_origem_nome)
         REFERENCES projetobd.pais_origem (nome)
@@ -17,52 +17,50 @@ CREATE TABLE IF NOT EXISTS projetobd.destilaria
 
 CREATE TABLE IF NOT EXISTS projetobd.loja
 (
-    nome VARCHAR(50),
-    url VARCHAR(50) NOT NULL,
+    nome TEXT,
+    url TEXT NOT NULL,
 	CONSTRAINT pk_loja PRIMARY KEY (nome)
 );
 
 CREATE TABLE IF NOT EXISTS projetobd.whisky
 (
     id SERIAL,
-    idade INT NOT NULL,
-    nome VARCHAR(50) NOT NULL,
-    teor_alcolico DECIMAL(2) NOT NULL,
-    destilaria_nome VARCHAR(50) NOT NULL,
-    pais_origem_nome VARCHAR(50) NOT NULL,
+    nome TEXT NOT NULL,
+    idade TEXT,
+    teor_alcolico TEXT,
+    destilaria_nome TEXT,
+    pais_origem_nome TEXT,
 	CONSTRAINT pk_whisky PRIMARY KEY (id),
     CONSTRAINT fk_whisky_destilaria FOREIGN KEY (destilaria_nome)
         REFERENCES projetobd.destilaria (nome),
     CONSTRAINT fk_whisky_pais_origem FOREIGN KEY (pais_origem_nome)
-        REFERENCES projetobd.pais_origem (nome),
-    CHECK (teor_alcolico >= 0)
+        REFERENCES projetobd.pais_origem (nome)
 );
 
 CREATE TABLE IF NOT EXISTS projetobd.historico
 (
     whisky_id INT,
-    loja_nome VARCHAR(50),
-    preco_sem_desconto DECIMAL(2) NOT NULL,
-    preco_com_desconto DECIMAL(2),
-    acessado_em DATE NOT NULL,
+    loja_nome TEXT,
+    preco_sem_desconto TEXT NOT NULL,
+    preco_com_desconto TEXT,
+    acessado_em TIMESTAMP NOT NULL,
 	CONSTRAINT pk_historico PRIMARY KEY (whisky_id, loja_nome, acessado_em),
     CONSTRAINT fk_historico_whisky FOREIGN KEY (whisky_id)
         REFERENCES projetobd.whisky (id),
     CONSTRAINT fk_historico_loja FOREIGN KEY (loja_nome)
-        REFERENCES projetobd.loja (nome),
-    CHECK (preco_sem_desconto >= 0 AND preco_com_desconto >= 0)
+        REFERENCES projetobd.loja (nome)
 );
 
 CREATE TABLE IF NOT EXISTS projetobd.ingrediente
 (
-    nome VARCHAR(50),
+    nome TEXT,
     CONSTRAINT pk_ingrediente PRIMARY KEY (nome)
 );
 
 CREATE TABLE IF NOT EXISTS projetobd.destilaria_utiliza_ingrediente
 (
-    destilaria_nome VARCHAR(50),
-    ingrediente_nome VARCHAR(50),
+    destilaria_nome TEXT,
+    ingrediente_nome TEXT,
     CONSTRAINT pk_destilaria_utiliza_ingrediente PRIMARY KEY (destilaria_nome, ingrediente_nome),
     CONSTRAINT fk_destilaria_utiliza_ingrediente_destilaria FOREIGN KEY (destilaria_nome)
         REFERENCES projetobd.destilaria (nome),
@@ -72,8 +70,8 @@ CREATE TABLE IF NOT EXISTS projetobd.destilaria_utiliza_ingrediente
 
 CREATE TABLE IF NOT EXISTS projetobd.script
 (
-    loja_nome VARCHAR(50),
-    data_insercao DATE,
+    loja_nome TEXT,
+    data_insercao TIMESTAMP,
     codigo TEXT,
 	CONSTRAINT pk_loja_vende_whisky PRIMARY KEY (loja_nome, data_insercao),
     CONSTRAINT fk_loja_vende_whisky_loja FOREIGN KEY (loja_nome)
@@ -82,9 +80,9 @@ CREATE TABLE IF NOT EXISTS projetobd.script
 
 CREATE TABLE IF NOT EXISTS projetobd.execucao_scripts
 (
-    script_loja_nome VARCHAR(50),
-    script_data_insercao DATE,
-    data_execucao DATE,
+    script_loja_nome TEXT,
+    script_data_insercao TIMESTAMP,
+    data_execucao TIMESTAMP,
     CONSTRAINT pk_execucao_scripts PRIMARY KEY (script_loja_nome, script_data_insercao, data_execucao),
     CONSTRAINT fk_execucao_scripts_script_loja_nome FOREIGN KEY (script_loja_nome, script_data_insercao)
 		REFERENCES projetobd.script(loja_nome, data_insercao)
