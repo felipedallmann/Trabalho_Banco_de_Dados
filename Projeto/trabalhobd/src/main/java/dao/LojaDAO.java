3a587584e1ab6801c3ed5dba1597fddb4048a830
Olavo Nascimento <olavo.nascimento4@gmail.com>
2022-05-04 10:46:06 -0300

    Atualizar model e DAO loja.

--- Projeto/trabalhobd/src/main/java/dao/LojaDAO.java
+++ Projeto/trabalhobd/src/main/java/dao/LojaDAO.java
@@ -1,18 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package dao;
 
 import java.sql.SQLException;
 import model.Loja;
 
-/**
- *
- * @author dskaster
- */
 public interface LojaDAO extends DAO<Loja> {
-
     public Loja getByName(String name) throws SQLException;
-    
 }
--- Projeto/trabalhobd/src/main/java/model/Loja.java
+++ Projeto/trabalhobd/src/main/java/model/Loja.java
@@ -1,17 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
 import com.google.gson.annotations.SerializedName;
 
-/**
- *
- * @author Usuario
- */
 public class Loja {
-    @SerializedName("Fabricante") private String nome;
+    @SerializedName("Fabricante")
+    private String nome;
     private String URL;
     private transient int itensVendidos;
     private transient int precoMedio;
@@ -48,7 +41,9 @@ public class Loja {
         this.URL = URL;
     }
 
- 
-    
-    
+    @Override
+    public String toString() {
+        return "Loja{" + "nome=" + nome + ", URL=" + URL + ", itensVendidos=" + itensVendidos + ", precoMedio="
+                + precoMedio + '}';
+    }
 }
